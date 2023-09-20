d3384c217f67c22a79a1cfad420a2441c92a9dbf
Q2k <q2k@q2k.dev>
2023-09-20 17:28:25 +0700

    change admin templates

--- src/admin.py
+++ src/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
 from src.models import Customer, Project
 
@@ -12,12 +14,27 @@ class ProjectInline(admin.TabularInline):
 
 class ProjectAdmin(admin.ModelAdmin):
     sortable_by = ("name", "owner", "created_at", "updated_at")
-    list_display = ("name", "project_id", "secret_key", "owner", "description", "created_at", "updated_at")
-    fields = ("name", "owner", "description", "project_id", "secret_key", ("created_at", "updated_at"))
-    readonly_fields = ("owner", "project_id", "secret_key", "created_at", "updated_at")
+    list_display = ("name", "project_id", "secret_key", "owner_display", "description", "created_at", "updated_at")
     search_fields = ("name", "owner__email", "id", "secret_key")
     list_filter = ("owner",)
     list_select_related = ("owner",)
+
+    def owner_display(self, obj):
+        link = reverse("admin:src_customer_change", args=[obj.owner.id])
+        return format_html('<b><a href="{}">{}</a></b>', link, obj.owner.email)
+    owner_display.short_description = "Owner"
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ("owner_display", "project_id", "secret_key", "created_at", "updated_at")
+        else:
+            return ("project_id", "secret_key")
+        
+    def get_fields(self, request, obj=None):
+        if obj:
+            return ("name", "owner_display", "description", "project_id", "secret_key", ("created_at", "updated_at"))
+        else:
+            return ("name", "owner", "description")
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ("id", "email", "projects_count", "created_at", "updated_at")
