86f393843d97d3d8e971a810101b8c676b8dccf7
Q2k <q2k@q2k.dev>
2023-09-22 13:10:35 +0700

    add error log handle

--- src/middleware/log.py
+++ src/middleware/log.py
@@ -20,3 +20,17 @@ class RequestLoggerMiddleware(MiddlewareMixin):
         customer_id = request.customer.pk if request.customer else 'Anonymous'
 
         logging.info(f"IP: {ip} - CustomerID: {customer_id} - {request.method} - {request.path}")
+
+    def process_exception(self, request, exception):
+        try:
+            LOGGING_EXCEPT_PATHS = settings.LOGGING_EXCEPT_PATHS
+        except:
+            LOGGING_EXCEPT_PATHS = ()
+
+        if resolve(request.path_info).app_name in LOGGING_EXCEPT_PATHS:
+            return None
+        
+        ip, _ = get_client_ip(request)
+        customer_id = request.customer.pk if request.customer else 'Anonymous'
+
+        logging.error(f"IP: {ip} - CustomerID: {customer_id} - {request.method} - {request.path} - {exception}")
