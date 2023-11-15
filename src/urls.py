64544d352de7e5796398fc529d11e2965099bc2a
Q2k <q2k@q2k.dev>
2023-11-15 17:31:14 +0700

    [change] reformat urls

--- src/urls.py
+++ src/urls.py
@@ -9,11 +9,11 @@ from src.admin import admin_site
 urlpatterns = [
     path("", index, name="index"),
     path("api/refresh_secret_key/<str:project>/", refresh_secret_key, name="refresh_secret_key"),
+    path("dashboard/password_reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("dashboard/password_reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("dashboard/password_reset/<str:token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("dashboard/", admin_site.urls),
     path("signup/", signup, name="signup"),
     path("verify", verify_email, name="verify"),
     path("token/<str:secret_key>", create_example_token, name="create_example_token"),
-    path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
-    path("password_reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("password_reset/<str:token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 ]
